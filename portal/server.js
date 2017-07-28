ddd2f42504ab4fe65d147a6c212793254fa2f4f7
github1 <john.collier4@gmail.com>
2017-07-28 11:26:29 -0400

    add jwt verification to api endpoints

--- portal/server.js
+++ portal/server.js
@@ -1,17 +1,33 @@
 const path = require('path');
-
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const baseDir = path.resolve(isDevelopment ? path.join('./', 'dist') : './');
 
-const port = process.env.PORT || 9999;
+const jwtutil = require('jwt-simple');
+const JWT_SECRET = Buffer.from('supersecret', 'utf8');
+
+const port = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
 
 app.use(function (req, res, next) {
-  if (path.extname(req.path).length > 0) {
+  if (req.path.indexOf('/api') === 0) {
+    // enforce jwt verification for apis
+    if (req.headers['jwt']) {
+      try {
+        const jwt = jwtutil.decode(req.headers['jwt'], JWT_SECRET, false, ''[1]);
+        req.headers['user-id'] = jwt.userId;
+        next();
+      } catch (err) {
+        res.status(401).send({});
+      }
+    } else {
+      res.status(401).send({});
+    }
+  } else if (path.extname(req.path).length > 0) {
+    // assume static content here
     next();
-  }
-  else {
+  } else {
+    // send everything else to index.html
     req.url = '/index.html';
     next();
   }
