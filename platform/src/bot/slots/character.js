9b3e5ad4eaa1e0120c4d65790b61339a0ed8c900
github1 <john.collier4@gmail.com>
2017-08-09 15:05:57 -0400

    fix file case

---
+++ platform/src/bot/slots/character.js
@@ -0,0 +1,14 @@
+let character = {
+  name: "character",
+  description: "a slot to hold single characters",
+  enumerationValues: [
+    {value: "a"},
+    {value: "b"},
+    {value: "c"},
+    {value: "e"},
+    {value: "o"},
+    {value: "i"}
+  ]
+};
+
+module.exports = character;
---
+++ platform/src/bot/slots/topic.js
@@ -0,0 +1,23 @@
+let topic = {
+  name: "topic",
+  description: "Topic of a question being asked by a customer",
+  enumerationValues: [
+    {
+      value: "pay my bill"
+    },
+    {
+      value: "change my address"
+    },
+    {
+      value: "change my mailing preferences"
+    },
+    {
+      value: "change my credit limit"
+    },
+    {
+      value: "make a complaint"
+    }
+  ]
+};
+
+module.exports = topic;
