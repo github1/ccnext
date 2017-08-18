1e3def8578a9cbc0d7e40f95939d2e6a1909844e
ELWOOD29 <graemecottrell94@hotmail.com>
2017-08-18 11:01:13 +0100

    Extended fulfillment processor to cover all intents Added constants for the fulfillment processor responses

--- platform/src/impl/consts.js
+++ platform/src/impl/consts.js
@@ -1,1 +1,15 @@
 export const WELCOME_MESSAGE = `Hi there, I'm here to help. You can ask me questions about your account such as "What is my balance?" or questions about our services like "How can I pay my bill?". What can I do for you today?`;
+export const TRANSACTIONS = [
+    {
+        vendor: "Tesco",
+        amount: "£23.12"
+    },
+    {
+        vendor: "Costa",
+        amount: "£3.50"
+    },
+    {
+        vendor: "Tfl",
+        amount: "£2.50"
+    }
+];
