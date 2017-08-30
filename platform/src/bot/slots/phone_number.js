9e97592f36b246346a2c38d8dca29cf66cea2758
ELWOOD29 <graemecottrell94@hotmail.com>
2017-08-30 14:34:56 +0100

    Created phone_number slot and request_callback intent, Added to bot_builder, Updated CCaaS to take intents from lex model, Created alternative request_processor lambda function

---
+++ platform/src/bot/slots/phone_number.js
@@ -0,0 +1,10 @@
+let phoneNumber = {
+  name : "phoneNumber",
+  description : "Slot to hold a phone number",
+  enumerationValues : [
+    { value: "07123456789"},
+    { value: "+447123456789"}
+  ]
+};
+
+module.exports = phoneNumber;
