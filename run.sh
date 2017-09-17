326894161d424ca4f3131061301466eb0a6140c6
github1 <john.collier4@gmail.com>
2017-09-16 23:54:54 -0400

    use dropler for cloud deployment

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export PUBLIC_URL="http://${DP_IP_ADDR}"
+
+docker-compose -f /${DP_NAME}/docker-compose.yml rm -f
+docker-compose -f /${DP_NAME}/docker-compose.yml build
+docker-compose -f /${DP_NAME}/docker-compose.yml up
